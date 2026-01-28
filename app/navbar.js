54d6dc60eb0c49287958f0f7c64640e24701a7b1
LAN Apps Studio <lanstudio.ser25@gmail.com>
2026-01-28 21:53:00 +0800

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -179,10 +179,11 @@ const navbarHTML = `
             <span class="dropbtn"><b>網頁應用程式 ▾</b></span>
             <div class="dropdown-content">
                 <a href="/apps.html"><b>所有網頁程式</b></a>
+                <a href="#"  class="ai-notice-navbar">最近更新 ▾▾▾</a>
+                <a href="/app/note.html"><b>雲端筆記</b></a>
                 <a href="#"  class="ai-notice-navbar">最新 ▾▾▾</a>
+                <a href="/tool/pomodoro.html"><b>新版番茄鐘</b></a>
                 <a href="/tool/time.html"><b>現在時間</b></a>
-                <a href="/tool/fullscreen.html"><b>全螢幕顏色</b></a>
-                <a href="/tool/classroom.html"><b>抽號器</b></a>
             </div>
         </li>
         <li><a href="/news.html"><b>最新消息</b></a></li>
@@ -228,14 +229,6 @@ const navbarHTML = `
         <span></span><span></span><span></span>
     </div>
 </nav>
-
-<div id="announcement-bar"  style="display: none;">
-    <div class="bar-content">No message</div>
-    <div class="bar-actions">
-        <a href="No message" class="btn-bar-go">No message</a>
-        <button class="btn-bar-close" id="close-bar">×</button>
-    </div>
-</div>
 `;
 
 let pageTitle = document.title.split('-')[0].trim();
@@ -491,4 +484,5 @@ onAuthStateChanged(auth, (user) => {
 })();
 
 export { auth, app };
+
 
