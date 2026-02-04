15fe91ccdb93c2196a4a5b0b9b4d3abeb914724a
LAN Apps Studio <lanstudio.ser25@gmail.com>
2026-02-04 11:55:04 +0800

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -179,6 +179,7 @@ const navbarHTML = `
             <span class="dropbtn"><b>網頁應用程式 ▾</b></span>
             <div class="dropdown-content">
                 <a href="/apps.html"><b>所有網頁程式</b></a>
+                <a href="/lan.appstore.html"><b>網頁程式商店</b></a>
                 <a href="#"  class="ai-notice-navbar">最近更新 ▾▾▾</a>
                 <a href="/app/note.html"><b>雲端筆記</b></a>
                 <a href="#"  class="ai-notice-navbar">最新 ▾▾▾</a>
@@ -493,3 +494,4 @@ onAuthStateChanged(auth, (user) => {
 
 
 export { auth, app };
+
